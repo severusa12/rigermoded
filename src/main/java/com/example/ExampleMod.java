74308291320faf2cd1d4c7b25e90961b80bb4405
severusa12 <116972370+severusa12@users.noreply.github.com>
2026-04-30 21:47:22 +0400

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,24 +1,25 @@
-package com.example;
+package com.example.mod;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+public class RigMod implements ModInitializer {
+    public static boolean isRigged = false;
+    private static KeyBinding toggleKey;
 
-public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
+    @Override
+    public void onInitialize() {
+        toggleKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            \"key.rigmod.toggle\", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_RIGHT_SHIFT, \"category.rigmod\"));
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-	}
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (toggleKey.wasPressed()) {
+                isRigged = !isRigged;
+            }
+        });
+    }
 }
